fead8f162b58f2a027d5be6848dc2cdd06c9a9a3
fosmjo <imefangjie@gmail.com>
2023-04-05 12:20:24 +0800

    feat: remove original data of unused modules

--- cleaner.go
+++ cleaner.go
@@ -1,9 +1,12 @@
 package cleaner
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,24 +18,31 @@ import (
 )
 
 type Cleaner struct {
-	modCachePath string
-	modfilePaths []string
+	modCachePath    string
+	modDownloadPath string
+	modfilePaths    []string
 }
 
 func New(modCachePath string, modfilePaths []string) *Cleaner {
 	return &Cleaner{
-		modCachePath: modCachePath,
-		modfilePaths: modfilePaths,
+		modCachePath:    modCachePath,
+		modDownloadPath: filepath.Join(modCachePath, "cache", "download"),
+		modfilePaths:    modfilePaths,
 	}
 }
 
 func (c *Cleaner) Clean() error {
-	cachedMods, err := c.allCachedMods()
+	extractedMods, err := c.allExtractedMods()
 	if err != nil {
 		return err
 	}
 
-	modfiles, err := c.modfiles()
+	downloadedMods, err := c.allDownloadedMods()
+	if err != nil {
+		return err
+	}
+
+	modfiles, err := c.allModfiles()
 	if err != nil {
 		return err
 	}
@@ -42,15 +52,10 @@ func (c *Cleaner) Clean() error {
 		return err
 	}
 
-	unusedMods := make([]string, 0, max(len(cachedMods)-len(inUseMods), 0))
+	unusedExtractedMods := c.unusedMods(extractedMods, inUseMods)
+	unusedDownloadedMods := c.unusedMods(downloadedMods, inUseMods)
 
-	for _, mod := range cachedMods {
-		if _, ok := inUseMods[mod]; !ok {
-			unusedMods = append(unusedMods, mod)
-		}
-	}
-
-	totalSize, err := c.calculateSize(unusedMods)
+	totalSize, err := c.calculateSize(unusedExtractedMods, unusedDownloadedMods)
 	if err != nil {
 		return err
 	}
@@ -64,7 +69,7 @@ You can:
 (3) Quit.
 
 Type one of the numbers in parentheses:`,
-		len(unusedMods),
+		len(unusedExtractedMods)+len(unusedDownloadedMods),
 		humanize.Bytes(totalSize),
 	)
 	var input string
@@ -75,36 +80,119 @@ Type one of the numbers in parentheses:`,
 
 	switch input {
 	case "1":
-		return c.removeMods(unusedMods)
+		return c.removeMods(unusedExtractedMods, unusedDownloadedMods)
 	case "2":
-		return c.viewMods(unusedMods)
+		return c.viewMods(unusedExtractedMods, unusedDownloadedMods)
 	default:
 		return nil
 	}
 }
 
-func (c *Cleaner) viewMods(mods []string) error {
-	for _, mod := range mods {
-		path := c.modAbsPath(mod)
+func (c *Cleaner) viewMods(extractedMods []string, downloadedMods []string) error {
+	for _, mod := range extractedMods {
+		path := c.extractedModAbsPath(mod)
 		fmt.Println(path)
 	}
+
+	for _, mod := range downloadedMods {
+		files, err := c.downloadedModFiles(mod)
+		if err != nil {
+			return err
+		}
+
+		for _, file := range files {
+			fmt.Println(file)
+		}
+	}
+
 	return nil
 }
 
-func (c *Cleaner) removeMods(mods []string) error {
-	for _, mod := range mods {
-		path := c.modAbsPath(mod)
+func (c *Cleaner) removeMods(extractedMods []string, downloadedMods []string) error {
+	for _, mod := range extractedMods {
+		path := c.extractedModAbsPath(mod)
 		fmt.Printf("Removing %s\n", path)
 		err := os.RemoveAll(path)
 		if err != nil {
 			return err
 		}
 	}
+
+	for _, mod := range downloadedMods {
+		files, err := c.downloadedModFiles(mod)
+		if err != nil {
+			return err
+		}
+
+		for _, file := range files {
+			fmt.Printf("Removing %s\n", file)
+			err := os.Remove(file)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return c.rewriteVersionList(downloadedMods)
+}
+
+func (c *Cleaner) rewriteVersionList(removedMods []string) error {
+	mod2versions := make(map[string][]string, len(removedMods))
+
+	for _, mod := range removedMods {
+		parts := strings.Split(mod, "@")
+		if len(parts) != 2 {
+			return fmt.Errorf("invalid mod: %s", mod)
+		}
+
+		mod2versions[parts[0]] = append(mod2versions[parts[0]], parts[1])
+	}
+
+	for mod, removedVersions := range mod2versions {
+		filepath := filepath.Join(c.modDownloadPath, mod, "@v", "list")
+		f, err := os.OpenFile(filepath, os.O_RDWR, 0644)
+		if err != nil {
+			continue
+		}
+
+		err = func() error {
+			defer f.Close()
+
+			allVersions, err := c.parseVersionListFile(f)
+			if err != nil {
+				return err
+			}
+
+			err = f.Truncate(0)
+			if err != nil {
+				return err
+			}
+
+			_, err = f.Seek(0, io.SeekStart)
+			if err != nil {
+				return err
+			}
+
+			remainedVersions := diffSlice(allVersions, removedVersions)
+			for _, version := range remainedVersions {
+				fmt.Fprintf(f, "%s\n", version)
+			}
+
+			return nil
+		}()
+
+		if err != nil {
+			log.Printf("failed to rewrite version list for %s: %v", mod, err)
+			continue
+		}
+	}
+
 	return nil
 }
 
-func (c *Cleaner) allCachedMods() ([]string, error) {
+func (c *Cleaner) allExtractedMods() ([]string, error) {
 	mods := make([]string, 0, 128)
+
 	err := filepath.WalkDir(c.modCachePath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -128,36 +216,96 @@ func (c *Cleaner) allCachedMods() ([]string, error) {
 
 		return nil
 	})
+
+	return mods, err
+}
+
+// allDownloadedMods returns the list of all mods under directory $GOPATH/pkg/mod/cache/download/
+func (c *Cleaner) allDownloadedMods() ([]string, error) {
+	store := make(map[string]struct{}, 256)
+
+	err := filepath.WalkDir(c.modDownloadPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() && d.Name() == "sumdb" {
+			return filepath.SkipDir
+		}
+
+		if !d.IsDir() && strings.Contains(path, "@v") && strings.Contains(d.Name(), ".") {
+			// e.g.: path => go.uber.org/fx/@v/v1.17.0.info
+			relpath, err := filepath.Rel(c.modDownloadPath, path)
+			if err != nil {
+				return err
+			}
+
+			// e.g. go.uber.org/fx/@v/v1.17.0.info => go.uber.org/fx
+			modpath := filepath.Dir(filepath.Dir(relpath))
+
+			filename := filepath.Base(relpath)
+			version := strings.TrimSuffix(filename, filepath.Ext(filename))
+
+			mod := modpath + "@" + version
+			store[mod] = struct{}{}
+		}
+
+		return nil
+	})
 	if err != nil {
 		return nil, err
+	}
+
+	mods := make([]string, 0, len(store))
+
+	for mod := range store {
+		mods = append(mods, mod)
 	}
 
 	return mods, nil
 }
 
-func (c *Cleaner) modAbsPath(mod string) string {
+func (c *Cleaner) unusedMods(mods []string, inUseMods map[string]struct{}) []string {
+	unused := make([]string, 0, max(len(mods)-len(inUseMods), 0))
+
+	for _, mod := range mods {
+		if _, ok := inUseMods[mod]; !ok {
+			unused = append(unused, mod)
+		}
+	}
+
+	return unused
+}
+
+func (c *Cleaner) extractedModAbsPath(mod string) string {
 	return filepath.Join(c.modCachePath, mod)
 }
 
-func (c *Cleaner) calculateSize(mods []string) (uint64, error) {
+func (c *Cleaner) calculateSize(extractedMods, downloadedMods []string) (uint64, error) {
 	var size atomic.Uint64
 	var wg sync.WaitGroup
 	errCh := make(chan error)
 
-	wg.Add(len(mods))
+	wg.Add(len(extractedMods) + len(downloadedMods))
 
-	for _, mod := range mods {
-		go func(mod string) {
-			defer wg.Done()
+	calculateModSize := func(mod string, calculate func(mod string) (int64, error)) {
+		defer wg.Done()
 
-			s, err := c.calculateModSize(mod)
-			if err != nil {
-				errCh <- err
-				return
-			}
+		s, err := calculate(mod)
+		if err != nil {
+			errCh <- err
+			return
+		}
 
-			size.Add(uint64(s))
-		}(mod)
+		size.Add(uint64(s))
+	}
+
+	for _, mod := range extractedMods {
+		go calculateModSize(mod, c.calculateExtractedModSize)
+	}
+
+	for _, mod := range downloadedMods {
+		go calculateModSize(mod, c.calculateDownloadedModSize)
 	}
 
 	go func() {
@@ -177,19 +325,70 @@ func (c *Cleaner) calculateSize(mods []string) (uint64, error) {
 	return size.Load(), nil
 }
 
-func (c *Cleaner) calculateModSize(mod string) (int64, error) {
+func (c *Cleaner) calculateExtractedModSize(mod string) (int64, error) {
 	var size int64
-	dir := c.modAbsPath(mod)
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
 
-		size += info.Size()
-		return nil
-	})
+	err := filepath.Walk(
+		c.extractedModAbsPath(mod),
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			size += info.Size()
+			return nil
+		},
+	)
 
 	return size, err
+}
+
+func (c *Cleaner) calculateDownloadedModSize(mod string) (int64, error) {
+	files, err := c.downloadedModFiles(mod)
+	if err != nil {
+		return 0, err
+	}
+
+	var size int64
+
+	for _, file := range files {
+		fi, err := os.Stat(file)
+		if err != nil {
+			return 0, err
+		}
+
+		size += fi.Size()
+	}
+
+	return size, err
+}
+
+func (c *Cleaner) downloadedModFiles(mod string) ([]string, error) {
+	parts := strings.Split(mod, "@")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid mod %s", mod)
+	}
+
+	modpath, version := parts[0], parts[1]
+
+	files := make([]string, 0, 6)
+
+	err := filepath.WalkDir(
+		filepath.Join(c.modDownloadPath, modpath, "@v"),
+		func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if strings.Contains(d.Name(), version) {
+				files = append(files, path)
+			}
+
+			return nil
+		},
+	)
+
+	return files, err
 }
 
 func (c *Cleaner) allInUseMods(modfiles []string) (map[string]struct{}, error) {
@@ -209,7 +408,7 @@ func (c *Cleaner) allInUseMods(modfiles []string) (map[string]struct{}, error) {
 	return result, nil
 }
 
-func (c *Cleaner) modfiles() ([]string, error) {
+func (c *Cleaner) allModfiles() ([]string, error) {
 	var modfiles []string
 
 	for _, p := range c.modfilePaths {
@@ -229,14 +428,26 @@ func (c *Cleaner) modfiles() ([]string, error) {
 	return modfiles, nil
 }
 
-func (c *Cleaner) parseModFile(modfilepath string) ([]string, error) {
-	data, err := os.ReadFile(modfilepath)
+func (c *Cleaner) parseModFile(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	filename := filepath.Base(modfilepath)
+	filename := filepath.Base(path)
 	return c.retriveMods(filename, data)
+}
+
+// parseVersionListFile parses a version list file into a slice of strings
+func (c *Cleaner) parseVersionListFile(r io.Reader) ([]string, error) {
+	scanner := bufio.NewScanner(r)
+	versions := make([]string, 0, 3)
+
+	for scanner.Scan() {
+		versions = append(versions, scanner.Text())
+	}
+
+	return versions, scanner.Err()
 }
 
 func (c *Cleaner) retriveMods(filename string, data []byte) ([]string, error) {
@@ -256,6 +467,23 @@ func (c *Cleaner) retriveMods(filename string, data []byte) ([]string, error) {
 	}
 
 	return mods, nil
+}
+
+// diffSlice returns a slice of strings that are in a but not in b
+func diffSlice(a, b []string) []string {
+	bmap := make(map[string]struct{}, len(b))
+	for _, v := range b {
+		bmap[v] = struct{}{}
+	}
+
+	result := make([]string, 0, max(len(a)-len(b), 0))
+	for _, v := range a {
+		if _, ok := bmap[v]; !ok {
+			result = append(result, v)
+		}
+	}
+
+	return result
 }
 
 func max(a, b int) int {
